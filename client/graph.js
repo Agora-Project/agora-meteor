83252e786e4311971b8b873cb46d9e23a9171023
Angle <Ribbonwing@gmail.com>
2016-08-29 15:42:28 -0600

    Got mdl working again, starting over on attempts at a menu.

--- client/graph.js
+++ client/graph.js
@@ -185,104 +185,6 @@ function linksToD3Array(linksCol, nodesCol) {
     return result;
 };
 
-function contextMenu() {
-    var height,
-        width,
-        margin = 0.1, // fraction of width
-        items = [],
-        rescale = false,
-        style = {
-            'rect': {
-                'mouseout': {
-                    'fill': 'rgb(244,244,244)',
-                    'stroke': 'white',
-                    'stroke-width': '1px'
-                },
-                'mouseover': {
-                    'fill': 'rgb(200,200,200)'
-                }
-            },
-            'text': {
-                'fill': 'steelblue',
-                'font-size': '13'
-            }
-        };
-
-    function menu(x, y) {
-        d3.select('.context-menu').remove();
-        scaleItems();
-
-        // Draw the menu
-        d3.select('svg')
-            .append('g').attr('class', 'context-menu')
-            .selectAll('tmp')
-            .data(items).enter()
-            .append('g').attr('class', 'menu-entry')
-            .style({'cursor': 'pointer'})
-            .on('mouseover', function(){
-                d3.select(this).select('rect').style(style.rect.mouseover) })
-            .on('mouseout', function(){
-                d3.select(this).select('rect').style(style.rect.mouseout) });
-
-        d3.selectAll('.menu-entry')
-            .append('rect')
-            .attr('x', x)
-            .attr('y', function(d, i){ return y + (i * height); })
-            .attr('width', width)
-            .attr('height', height)
-            .style(style.rect.mouseout);
-
-        d3.selectAll('.menu-entry')
-            .append('text')
-            .text(function(d){ return d; })
-            .attr('x', x)
-            .attr('y', function(d, i){ return y + (i * height); })
-            .attr('dy', height - margin / 2)
-            .attr('dx', margin)
-            .style(style.text);
-
-        // Other interactions
-        d3.select('body')
-            .on('click', function() {
-                d3.select('.context-menu').remove();
-            });
-
-    }
-
-    menu.items = function(e) {
-        if (!arguments.length) return items;
-        for (i in arguments) items.push(arguments[i]);
-        rescale = true;
-        return menu;
-    }
-
-    // Automatically set width, height, and margin;
-    function scaleItems() {
-        if (rescale) {
-            d3.select('svg').selectAll('tmp')
-                .data(items).enter()
-                .append('text')
-                .text(function(d){ return d; })
-                .style(style.text)
-                .attr('x', -1000)
-                .attr('y', -1000)
-                .attr('class', 'tmp');
-            var z = d3.selectAll('.tmp')[0]
-                      .map(function(x){ return x.getBBox(); });
-            width = d3.max(z.map(function(x){ return x.width; }));
-            margin = margin * width;
-            width =  width + 2 * margin;
-            height = d3.max(z.map(function(x){ return x.height + margin / 2; }));
-
-            // cleanup
-            d3.selectAll('.tmp').remove();
-            rescale = false;
-        }
-    }
-
-    return menu;
-}
-
 function ForumTree(forumIndex, nodes, links) {
 
   this.forumIndex = forumIndex;
@@ -317,7 +219,7 @@ function ForumTree(forumIndex, nodes, links) {
       .links(links)
       .gravity(0.10)
       .charge(-5000)
-      .friction(0.5)
+      .friction(0.9)
       .linkDistance(150)
       .on("tick", tick);
 
@@ -545,12 +447,7 @@ function ForumTree(forumIndex, nodes, links) {
         .attr("class", 'control expand-button')
         .attr("id", function(d) { return "expandButton-" + d.id;})
         .style("fill", "rebeccapurple")
-        .on("contextmenu", function (d) {
-            // Build the popup
-            d3.event.preventDefault();
-            var menu = contextMenu().items('first item', 'second option', 'whatever, man');
-            menu(d.x + document.getElementById("expandButton-"+ d.id).getBBox().x,
-                 d.y + document.getElementById("expandButton-"+ d.id).getBBox().y);
+        .on("click", function (d) {
 
 
 
@@ -564,8 +461,6 @@ function ForumTree(forumIndex, nodes, links) {
               handlers.addHandler(link.sourceId);
             });
         });
-
-    //console.log("Added expand buttons.");
 
     force.start();
   };
