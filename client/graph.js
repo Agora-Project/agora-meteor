a7a138865b9b3c406a0152e647dcf9aff762b7d1
Angle <Ribbonwing@gmail.com>
2016-08-29 20:50:33 -0600

    Attempting to implement menus in svg. Progress has been made.

--- client/graph.js
+++ client/graph.js
@@ -138,11 +138,7 @@ Template.forumIndex.rendered = function() {
   linksCursor.observe({
     added: function(doc) {
       if (init) { return; }
-      //console.log("Adding Link: " + doc._id);
-      if (nodesInGraph.contains(doc.sourceId))
-        handlers.addHandler(doc.targetId);
-      else if (nodesInGraph.contains(doc.targetId))
-        handlers.addHandler(doc.sourceId);
+      console.log("Adding Link: " + doc._id);
       if(tree.addLink(doc))
         tree.render();
     },
@@ -157,6 +153,103 @@ Template.forumIndex.rendered = function() {
   init = false;
 };
 
+function contextMenu() {
+    var height,
+        width,
+        margin = 0.1, // fraction of width
+        items = [],
+        rescale = false,
+        style = {
+            'rect': {
+                'mouseout': {
+                    'fill': 'rgb(244,244,244)',
+                    'stroke': 'white',
+                    'stroke-width': '1px'
+                },
+                'mouseover': {
+                    'fill': 'rgb(200,200,200)'
+                }
+            },
+            'text': {
+                'fill': 'steelblue',
+                'font-size': '13'
+            }
+        };
+
+    function menu(x, y) {
+        d3.select('.context-menu').remove();
+        scaleItems();
+
+        // Draw the menu
+        d3.select('svg')
+            .append('g').attr('class', 'context-menu')
+            .selectAll('tmp')
+            .data(items).enter()
+            .append('g').attr('class', 'menu-entry')
+            .style({'cursor': 'pointer'})
+            .on('mouseover', function(){
+                d3.select(this).select('rect').style(style.rect.mouseover) })
+            .on('mouseout', function(){
+                d3.select(this).select('rect').style(style.rect.mouseout) });
+
+        d3.selectAll('.menu-entry')
+            .append('rect')
+            .attr('x', x)
+            .attr('y', function(d, i){ return y + (i * height); })
+            .attr('width', width)
+            .attr('height', height)
+            .style(style.rect.mouseout);
+
+        d3.selectAll('.menu-entry')
+            .append('text')
+            .text(function(d){ return d; })
+            .attr('x', x)
+            .attr('y', function(d, i){ return y + (i * height); })
+            .attr('dy', height - margin / 2)
+            .attr('dx', margin)
+            .style(style.text);
+
+        // Other interactions
+        d3.select('body')
+            .on('click', function() {
+                //d3.select('.context-menu').remove();
+            });
+
+    }
+
+    menu.items = function(e) {
+        if (!arguments.length) return items;
+        for (i in arguments) items.push(arguments[i]);
+        rescale = true;
+        return menu;
+    }
+
+    // Automatically set width, height, and margin;
+    function scaleItems() {
+        if (rescale) {
+            d3.select('svg').selectAll('tmp')
+                .data(items).enter()
+                .append('text')
+                .text(function(d){ return d; })
+                .style(style.text)
+                .attr('x', -1000)
+                .attr('y', -1000)
+                .attr('class', 'tmp');
+            var z = d3.selectAll('.tmp')[0]
+                      .map(function(x){ return x.getBBox(); });
+            width = d3.max(z.map(function(x){ return x.width; }));
+            margin = margin * width;
+            width =  width + 2 * margin;
+            height = d3.max(z.map(function(x){ return x.height + margin / 2; }));
+
+            // cleanup
+            d3.selectAll('.tmp').remove();
+            rescale = false;
+        }
+    }
+
+    return menu;
+}
 
 function resetTargetsSelection() {
     Session.set('selectedTargets', {});
@@ -449,8 +542,9 @@ function ForumTree(forumIndex, nodes, links) {
         .style("fill", "rebeccapurple")
         .on("click", function (d) {
 
-
-
+            var menu = contextMenu().items('first item', 'second option', 'whatever, man');
+            d3.event.preventDefault();
+            menu(d3.mouse(d3.select("svg")[0][0])[0], d3.mouse(d3.select("svg")[0][0])[1]);
             Link.find({sourceId: d._id}).fetch().forEach(function(link) {
               nodesInGraph.add(link.targetId);
               handlers.addHandler(link.targetId);
