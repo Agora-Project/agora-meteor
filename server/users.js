3b1cee1ab3eb7bb8331c8f98242798e2e676e149
Angle <Ribbonwing@gmail.com>
2016-10-06 13:03:49 -0600

    Fixed a few more indentations.

--- server/users.js
+++ server/users.js
@@ -1,30 +1,30 @@
 Meteor.methods({
-  switchBanned: function (targetUserId, isBanned) {
-    var loggedInUser = Meteor.user()
+    switchBanned: function (targetUserId, isBanned) {
+        var loggedInUser = Meteor.user()
 
-    if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['moderator'])) {
-      throw new Meteor.Error(403, "Access denied")
-    }
+        if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['moderator'])) {
+            throw new Meteor.Error(403, "Access denied")
+        }
 
-    Meteor.users.update(targetUserId, {
-      $set: {isBanned: isBanned}
-    });
-    if (isBanned){
-      //Immediately logout user
-      Meteor.users.update({_id: targetUserId}, {$set : { "services.resume.loginTokens" : [] }})
-    }
-  },
-  switchModerator: function (targetUserId, isModerator) {
-    var loggedInUser = Meteor.user()
+        Meteor.users.update(targetUserId, {
+            $set: {isBanned: isBanned}
+        });
+        if (isBanned){
+            //Immediately logout user
+            Meteor.users.update({_id: targetUserId}, {$set : { "services.resume.loginTokens" : [] }})
+        }
+    },
+    switchModerator: function (targetUserId, isModerator) {
+        var loggedInUser = Meteor.user()
 
-    if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['moderator'])) {
-      throw new Meteor.Error(403, "Access denied")
-    }
+        if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['moderator'])) {
+            throw new Meteor.Error(403, "Access denied")
+        }
 
-    if (isModerator) {
-      Roles.addUsersToRoles(targetUserId, ['moderator']);
-    } else {
-      Roles.removeUsersFromRoles(targetUserId, ['moderator']);
+        if (isModerator) {
+            Roles.addUsersToRoles(targetUserId, ['moderator']);
+        } else {
+            Roles.removeUsersFromRoles(targetUserId, ['moderator']);
+        }
     }
-  }
 })
