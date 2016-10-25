bf27e54798562b74f288b780f05d2316bb7080aa
Angle <Ribbonwing@gmail.com>
2016-10-24 18:08:09 -0600

    Removed test reply post function and added empty submitButton functionality.

--- client/graph.js
+++ client/graph.js
@@ -44,13 +44,6 @@ Template.post.events({
         });
     },
     'click .replyButton': function(evt) {
-
-        var newReplyPost = {
-            title: 'Reply',
-            content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        }
-
-        Post.insert(newReplyPost);
     },
     'click .closeButton': function(evt) {
         tree.removeNode(this);
@@ -65,6 +58,8 @@ Template.reply.onRendered(function () {
 Template.reply.events({
     'click .closeButton': function(evt) {
         tree.removeNode(this);
+    },
+    'click .submitButton': function(evt) {
     }
 
 });
