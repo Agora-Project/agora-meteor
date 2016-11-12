2339b289a998703b1cc39db7fb03732c640bd901
Angle <Ribbonwing@gmail.com>
2016-11-12 15:22:59 -0700

    Fixed bug where switching to the forum page did not automatically dispay the root post.

--- client/graph.js
+++ client/graph.js
@@ -326,9 +326,116 @@ function ForumTree(forumIndex, nodesCursor, linksCursor) {
 
     //put nodes and links into D3-friendly arrays
     this.nodes = [];
+    this.links = [];
+
+
+
+    this.findNode = function(doc) {
+        if (doc._id)
+            return this.nodes.find(function(n) {return (doc._id == n._id)});
+        else return this.nodes.find(function(n) {return (doc == n._id)});
+    };
+
+    this.findLink = function(doc) {
+        if (doc._id)
+            return this.links.find(function(l) {return (doc._id == l._id)});
+
+        if (!doc.source || !doc.target) var link = linksToD3Array([doc], this.nodes)[0];
+        else var link = doc;
+        return this.links.find(function(l) {return (link.source == l.source && link.target == l.target)});
+    };
+
+    this.containsNode = function(doc) {
+        if (this.findNode(doc)) return true;
+        else return false;
+    };
+
+    this.containsLink = function(doc) {
+        if (this.findLink(doc))
+            return false;
+    };
+
+    this.addNode = function(doc) {
+        if (!this.nodes.find(function(n) {return (doc._id == n._id)})) {
+            let _id = doc._id;
+            if (!nodesInGraph.findOne({_id: doc._id}))
+                _id = nodesInGraph.insert(doc);
+
+            doc = nodesInGraph.findOne({_id: _id});
+            this.nodes.push(doc);
+
+            return this.nodes[this.nodes.length - 1];
+        }
+        return false;
+    };
+
+    this.addLink = function(doc) {
+        //DANGER!
+        if (!doc._id) {
+            var _id = nodesInGraph.insert(doc);
+            doc = nodesInGraph.findOne({_id: _id});
+        }
+        //THIS IS AN AWFUL HACK!
+        //We are inserting links that don't have _id's into the local
+        //nodesInGraph database purely for the purpose of getting them ids.
+
+        let link = linksToD3Array([doc], this.nodes)[0];
+        if (link && !this.containsLink(doc)) {
+            this.links.push(link);
+            this.runGraph();
+            this.render();
+            return true;
+        }
+        return false;
+    };
+
+    this.removeNode = function(doc) {
+        var iToRemove = -1;
+        if (this.nodes.length !== 0) {
+            this.nodes.forEach(function(node, i) {
+                if (node._id === doc._id) {
+                    iToRemove = i;
+                }
+            });
+        }
+        if (iToRemove != -1) {
+            for (i = 0; i < this.links.length;) {
+                link = this.links[i];
+                if (link.source._id === doc._id || link.target._id == doc._id)
+                    this.links.splice(i, 1);
+                else i++;
+            }
+            this.nodes.splice(iToRemove, 1);
+            nodesInGraph.remove({_id: doc._id});
+            this.runGraph();
+            this.render();
+            return true;
+        }
+        return false;
+    };
+
+    this.removeLink = function(doc) {
+        var iToRemove = -1;
+        this.links.forEach(function(link, i) {
+            if (link._id === doc._id) {
+                iToRemove = i;
+            } else if (link.source._id == doc.sourceId && link.target._id == doc.targetId) {
+                iToRemove = i;
+            }
+        });
+        if (iToRemove != -1) {
+            this.links.splice(iToRemove, 1);
+            this.runGraph();
+            this.render();
+            return true;
+        }
+        return false;
+    };
+
+    var tree = this;
     nodesCursor.forEach(function(n) {
-        n.selectable = true;
-        if (nodesInGraph.findOne({_id: n._id})) this.nodes.push(n);
+        n.type = "post";
+        tree.addNode(n);
     });
     this.links = linksToD3Array(linksCursor, this.nodes);
 
@@ -442,105 +549,5 @@ function ForumTree(forumIndex, nodesCursor, linksCursor) {
         });
     };
 
-    this.findNode = function(doc) {
-        if (doc._id)
-            return this.nodes.find(function(n) {return (doc._id == n._id)});
-        else return this.nodes.find(function(n) {return (doc == n._id)});
-    };
-
-    this.findLink = function(doc) {
-        if (doc._id)
-            return this.links.find(function(l) {return (doc._id == l._id)});
-
-        if (!doc.source || !doc.target) var link = linksToD3Array([doc], this.nodes)[0];
-        else var link = doc;
-        return this.links.find(function(l) {return (link.source == l.source && link.target == l.target)});
-    };
-
-    this.containsNode = function(doc) {
-        if (this.findNode(doc)) return true;
-        else return false;
-    };
-
-    this.containsLink = function(doc) {
-        if (this.findLink(doc))
-            return false;
-    };
-
-    this.addNode = function(doc) {
-        if (!this.nodes.find(function(n) {return (doc._id == n._id)})) {
-            let _id = doc._id;
-            if (!nodesInGraph.findOne({_id: doc._id}))
-                _id = nodesInGraph.insert(doc);
-
-            doc = nodesInGraph.findOne({_id: _id});
-            this.nodes.push(doc);
-
-            return this.nodes[this.nodes.length - 1];
-        }
-        return false;
-    };
-
-    this.addLink = function(doc) {
-        //DANGER!
-        if (!doc._id) {
-            var _id = nodesInGraph.insert(doc);
-            doc = nodesInGraph.findOne({_id: _id});
-        }
-        //THIS IS AN AWFUL HACK!
-        //We are inserting links that don't have _id's into the local
-        //nodesInGraph database purely for the purpose of getting them ids.
-
-        let link = linksToD3Array([doc], this.nodes)[0];
-        if (link && !this.containsLink(doc)) {
-            this.links.push(link);
-            this.runGraph();
-            this.render();
-            return true;
-        }
-        return false;
-    };
-
-    this.removeNode = function(doc) {
-        var iToRemove = -1;
-        if (this.nodes.length !== 0) {
-            this.nodes.forEach(function(node, i) {
-                if (node._id === doc._id) {
-                    iToRemove = i;
-                }
-            });
-        }
-        if (iToRemove != -1) {
-            for (i = 0; i < this.links.length;) {
-                link = this.links[i];
-                if (link.source._id === doc._id || link.target._id == doc._id)
-                    this.links.splice(i, 1);
-                else i++;
-            }
-            this.nodes.splice(iToRemove, 1);
-            nodesInGraph.remove({_id: doc._id});
-            this.runGraph();
-            this.render();
-            return true;
-        }
-        return false;
-    };
-
-    this.removeLink = function(doc) {
-        var iToRemove = -1;
-        this.links.forEach(function(link, i) {
-            if (link._id === doc._id) {
-                iToRemove = i;
-            } else if (link.source._id == doc.sourceId && link.target._id == doc.targetId) {
-                iToRemove = i;
-            }
-        });
-        if (iToRemove != -1) {
-            this.links.splice(iToRemove, 1);
-            this.runGraph();
-            this.render();
-            return true;
-        }
-        return false;
-    };
+    return this;
 }
