1478a28f5d699bfacba202a8f42fd9ab829c0f5c
Angle <Ribbonwing@gmail.com>
2016-11-26 13:58:40 -0700

    fixed missing rename.

--- routes.js
+++ routes.js
@@ -14,9 +14,9 @@ Router.route('/forum', {
 Router.route('/forum/post/:_id', function() {
     var routerThis = this;
     var id = this.params._id;
-    
-    this.wait(Meteor.subscribe('forum', id));
-    
+
+    this.wait(Meteor.subscribe('post', id));
+
     if (this.ready()) {
         var post = Post.findOne({_id: id});
         if (post) routerThis.render('expandedPost', {data: post});
