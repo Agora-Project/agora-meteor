0e6169200a2e995ac6af1cdf4633e6b1fd1430f5
Angle <Ribbonwing@gmail.com>
2016-12-30 16:42:21 -0700

    Added pan feature to overview.

--- client/overview/overview.js
+++ client/overview/overview.js
@@ -4,6 +4,14 @@
     License: GPL, Check file LICENSE
 */
 
+var unFocus = function () {
+    if (document.selection) {
+        document.selection.empty();
+    } else {
+        window.getSelection().removeAllRanges();
+    }
+}
+
 Template.overview.onCreated(function() {
 
     this.nodeDisplay = new Mongo.Collection(null);
@@ -25,6 +33,9 @@ Template.overview.onCreated(function() {
 
             let postArray = [];
             let linkArray = [];
+
+            overviewObject.postArray = postArray;
+            overviewObject.linkArray = linkArray;
 
             $.each(posts, function(id, post) {
                 if (post.data.links)
@@ -72,27 +83,60 @@ Template.overview.onCreated(function() {
             for (let i = 0; i < 256; i++) graph.tick();
             graph.stop();
 
-            for (let post of postArray) {
-                let div = post.div;
-                div.css("left", post.x - div.outerWidth()/2.0);
-                div.css("top", post.y - div.outerHeight()/2.0);
+            overviewObject.render = function() {
+                for (let post of postArray) {
+                    let div = post.div;
+                    div.css("left", post.x - div.outerWidth()/2.0);
+                    div.css("top", post.y - div.outerHeight()/2.0);
+                }
+
+                $('.overview-link').remove(); //TODO: don't redo all links upon change to graph
+                let svg = $('.overview-links-graph');
+
+                for (let link of linkArray) {
+                    $(document.createElementNS('http://www.w3.org/2000/svg','line'))
+                        .attr('class', 'overview-link')
+                        .attr('stroke', 'black')
+                        .attr('x1', link.source.x)
+                        .attr('y1', link.source.y)
+                        .attr('x2', link.target.x)
+                        .attr('y2', link.target.y)
+                        .appendTo(svg);
+                }
+
             }
 
-            $('.overview-link').remove(); //TODO: don't redo all links upon change to graph
-            let svg = $('.overview-links-graph');
-
-            for (let link of linkArray) {
-                $(document.createElementNS('http://www.w3.org/2000/svg','line'))
-                    .attr('class', 'overview-link')
-                    .attr('stroke', 'black')
-                    .attr('x1', link.source.x)
-                    .attr('y1', link.source.y)
-                    .attr('x2', link.target.x)
-                    .attr('y2', link.target.y)
-                    .appendTo(svg);
-            }
+            overviewObject.render();
         }
     });
+});
+
+Template.overview.events({
+    'mousedown, touchstart': function(event, template) {
+        if (event.button != 0) return;
+        template.dragging = true;
+        template.counter = 0;
+        template.mousePos = {x: event.screenX, y: event.screenY};
+    },
+    'mouseup, touchend': function(event, template) {
+        template.dragging = false;
+        overviewObject.render();
+    },
+    'mousemove, touchmove': function(event, template) {
+        if (template.dragging) {
+            unFocus();
+            for (let i = 0; i < overviewObject.postArray.length; i++) {
+                overviewObject.postArray[i].x += (event.screenX - template.mousePos.x);
+                overviewObject.postArray[i].y += (event.screenY - template.mousePos.y);
+            }
+            template.mousePos = {x: event.screenX, y: event.screenY};
+
+            if (template.counter <= 0) {
+                overviewObject.render();
+                template.counter = 2;
+            } else template.counter--;
+        }
+    }
 });
 
 Template.overview.helpers({
