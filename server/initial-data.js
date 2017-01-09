319e5d700357ff95cc931fbc1ceda3e5f9da95f1
Sam Johnson <x.smashmaster.x@gmail.com>
2017-01-09 15:41:21 -0700

    Implemented random posts generation

--- server/initial-data.js
+++ server/initial-data.js
@@ -5,7 +5,6 @@
 */
 
 Meteor.startup(function() {
-
     var moderatorEmail, moderatorId;
     if (!Post.findOne({$where : '!this.links || this.links.length < 1'})) {
         console.log("Adding root post");
@@ -13,7 +12,34 @@ Meteor.startup(function() {
             title: 'Forum Root',
             links: []
         });
+        
+        /*
+        console.log("Adding fake posts");
+        for (let i=0; i<128; i++) {
+            let posts = [];
+            Post.find().forEach(function(post) {
+                posts.push(post);
+            });
+            
+            //Sort by date.
+            posts.sort(function(a, b) {
+                return b.postedOn - a.postedOn;
+            });
+            
+            //Increase exponent to more strongly prefer replying to newer posts.
+            let random = Math.pow(Math.random(), 3.0);
+            let post = posts[Math.floor(random*posts.length)];
+            
+            let reply = {
+                title: 'Fake Post',
+                links: [{target: post._id}]
+            };
+            
+            Meteor.call('insertPost', reply);
+        }
+        */
     }
+    
     moderatorEmail = "moderator@example.com";
     if (!Meteor.users.findOne({
         "emails.address": moderatorEmail
