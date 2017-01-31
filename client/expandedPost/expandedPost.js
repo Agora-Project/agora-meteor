d529d9f7c6bffa3320d6f61058bde4451f2f5cc2
Angle <Ribbonwing@gmail.com>
2017-01-30 18:13:27 -0700

    Implemented hourglass view in expanded view.

--- client/expandedPost/expandedPost.js
+++ client/expandedPost/expandedPost.js
@@ -4,6 +4,15 @@
     License: GPL, Check file LICENSE
 */
 
+
+var unFocus = function () {
+    if (document.selection) {
+        document.selection.empty();
+    } else {
+        window.getSelection().removeAllRanges();
+    }
+}
+
 Template.expandedPost.onRendered(function () {
     var instance = Template.instance();
 
@@ -32,66 +41,183 @@ Template.expandedPost.helpers({
     }
 });
 
+Template.hourglassView.onCreated(function() {
 
-Template.expandedViewPostList.onCreated(function() {
-    let postList = this;
+    this.nodeDisplay = new Mongo.Collection(null);
 
-    this.posts = new Mongo.Collection(null);
+    overviewObject = this; // !! Global variable!
 
-    for (var i in this.data.links) {
-        let linkID = this.data.links[i].target;
-        handlers.addHandler(linkID, {
-            onReady: function() {
-                let doc = Post.findOne({_id: linkID});
-                if (!nodesInGraph.findOne({_id: doc._id}))
-                    postList.posts.insert(doc);
+    let postSubscription = this.subscribe('newestPosts', Date.now());
+
+    this.autorun(function() {
+        if (postSubscription.ready()) {
+            let posts = {}, postsToProcess = [overviewObject.data];
+            while (postsToProcess.length > 0) {
+                let post = postsToProcess[0];
+                if (!posts[post._id]) {
+                    posts[post._id] = {
+                        data: post,
+                        div: $('#hourglass-node-' + post._id)
+                    };
+                }
+                for (var i of post.links) {
+                    let linkID = i.target;
+                    let doc = Post.findOne({_id: linkID});
+                    postsToProcess.push(doc);
+
+                }
+                postsToProcess.splice(0,1);
             }
-        });
-
-    }
-    for (var i in this.data.replyIDs) {
-        let replyID = this.data.replyIDs[i];
-        handlers.addHandler(replyID,  {
-            onReady: function() {
-                let doc = Post.findOne({_id: replyID});
-                if (!nodesInGraph.findOne({_id: doc._id}))
-                    postList.posts.insert(doc);
+            postsToProcess = [overviewObject.data];
+            while (postsToProcess.length > 0) {
+                let post = postsToProcess[0];
+                posts[post._id] = {
+                    data: post,
+                    div: $('#hourglass-node-' + post._id)
+                };
+                for (var replyID of post.replyIDs) {
+                    let doc = Post.findOne({_id: replyID});
+                    postsToProcess.push(doc);
+                }
+                postsToProcess.splice(0,1);
             }
-        });
 
+            let postArray = [];
+            let linkArray = [];
+
+            $.each(posts, function(id, post) {
+                if (post.data.links)
+                for (let link of post.data.links) {
+                    if (link.target in posts) {
+                        linkArray.push({
+                            source: post,
+                            target: posts[link.target]
+                        });
+                    }
+                }
+                postArray.push(post);
+            });
+
+            //let layout = new GraphLayoutForce(postArray, linkArray);
+            let layout = new GraphLayoutLayered(postArray, linkArray);
+
+            overviewObject.postArray = layout.nodes;
+
+            overviewObject.render = function() {
+                for (let post of layout.nodes) {
+                    if (post.name !== undefined) {
+                        let div = post.name.div;
+                        div.css("left", post.x - div.outerWidth()/2.0);
+                        div.css("top", post.y - div.outerHeight()/2.0);
+                    }
+                }
+
+                $('.hourglass-link').remove(); //TODO: don't redo all links upon change to graph
+                let svg = $('.hourglass-links-graph');
+
+                for (let link of layout.links) {
+                    $(document.createElementNS('http://www.w3.org/2000/svg','line'))
+                        .attr('class', 'hourglass-link')
+                        .attr('stroke', 'black')
+                        .attr('x1', link.source.x)
+                        .attr('y1', link.source.y)
+                        .attr('x2', link.target.x)
+                        .attr('y2', link.target.y)
+                        .appendTo(svg);
+                }
+            }
+
+            overviewObject.render();
+        }
+    });
+});
+
+Template.hourglassView.events({
+    'mousedown, touchstart': function(event, template) {
+        if (event.button != 0) return;
+        template.dragging = true;
+        template.counter = 0;
+        template.mousePos = {x: event.screenX, y: event.screenY};
+    },
+    'mouseup, touchend': function(event, template) {
+        template.dragging = false;
+        overviewObject.render();
+    },
+    'mousemove, touchmove': function(event, template) {
+        if (template.dragging) {
+            unFocus();
+            for (let post of overviewObject.postArray) {
+                post.x += (event.screenX - template.mousePos.x);
+                post.y += (event.screenY - template.mousePos.y);
+            }
+            template.mousePos = {x: event.screenX, y: event.screenY};
+
+            //Horrible hack to improve performance.
+            //TODO: Replace with a requestAnimationFrame() callback.
+            if (template.counter <= 0) {
+                overviewObject.render();
+                template.counter = 3;
+            }
+            else {
+                template.counter--;
+            }
+        }
     }
 });
 
-Template.expandedViewPostList.onRendered(function() {
-});
-
-Template.expandedViewPostList.onDestroyed(function() {
-
-});
-
-Template.expandedViewPostList.helpers({
-    posts: function() {
-        return Template.instance().posts.find({});
+Template.hourglassView.helpers({
+    nodeDisplay: function() {
+        return Template.instance().nodeDisplay.find({});
+    },
+    nodes: function() {
+        return Post.find({}, {limit: 1000});
     }
 });
 
-Template.expandedViewPostList.events({
-    "click ": function(event) {
+Template.hourglassNode.events({
+    'mousedown .undraggable, touchstart .undraggable': function(event) {
+        if (event.button != 0) return;
         event.stopImmediatePropagation();
     },
-    "mousedown": function(event) {
-        event.stopImmediatePropagation();
+    "mouseenter": function(event) {
+        overviewObject.nodeDisplay.remove({});
+        overviewObject.nodeDisplay.insert(this);
+    },
+    "mouseleave": function(event) {
+        overviewObject.nodeDisplay.remove({});
     }
 });
 
-Template.expandedViewPostListing.helpers({
+Template.hourglassPost.onRendered(function () {
+    var instance = Template.instance();
+
+    var postLink = instance.$('.titleBar a');
+    postLink.attr('title', postLink.text());
+
+    var usernameLink = instance.$('.username');
+    usernameLink.attr('title', usernameLink.text());
+
+    if(this.data.content)
+        instance.$('.post-content').html(XBBCODE.process({
+            text: this.data.content,
+            removeMisalignedTags: false,
+            addInLineBreaks: true
+        }).html);
+
+    let post = instance.$(".post");
+    let point = $("#hourglass-node-" + this.data._id);
+    post.css("left", parseInt(point.css("left")) + 20).css("top", point.css("top"));
+
+});
+
+Template.hourglassPost.helpers({
     user: function() {
         return Meteor.users.findOne(this.posterID);
-    }
-});
-
-Template.expandedViewPostListing.events({
-    "mousedown": function(event) {
-        event.stopImmediatePropagation();
+    },
+    hasContent: function() {
+        return (this.content && this.content.length > 0);
+    },
+    age: function() {
+        return new Date(this.postedOn).toDateString();
     }
 });
