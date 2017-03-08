d41bf5100159e29a5b4535640cda057d371bf4bb
Sam Johnson <x.smashmaster.x@gmail.com>
2017-03-08 14:55:29 -0700

    Loading shader program

--- client/webglView/webglView.js
+++ client/webglView/webglView.js
@@ -22,22 +22,33 @@ Template.webglView.onCreated(function() {
     });
 });
 
-let vertexShaderSource = "\
+let VERT_SHADER_SOURCE = "\
 attribute vec2 in_pos;\
 void main(void) {\
     gl_Position = vec4(in_pos, 0.0, 1.0);\
 }";
 
-let fragmentShaderSource ="\
+let FRAG_SHADER_SOURCE ="\
 precision mediump float;\
 void main() {\
     gl_FragColor = vec4(1.0);\
 }";
 
+let loadShader = function(gl, source, type) {
+    let shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        throw gl.getShaderInfoLog(shader);
+    }
+    return shader;
+}
+
 Template.webglView.onRendered(function() {
     let canvas = $(".gl-viewport");
     let gl = canvas[0].getContext('experimental-webgl');
     
+    //Set up resize callback
     this.isDestroyed = false;
     let sizeDirty = true;
     
@@ -45,6 +56,19 @@ Template.webglView.onRendered(function() {
         sizeDirty = true;
     });
     
+    //Set up shader program
+    let vertShader = loadShader(gl, VERT_SHADER_SOURCE, gl.VERTEX_SHADER);
+    let fragShader = loadShader(gl, FRAG_SHADER_SOURCE, gl.FRAGMENT_SHADER);
+    let shader = gl.createProgram();
+    gl.attachShader(shader, vertShader);
+    gl.attachShader(shader, fragShader);
+    gl.linkProgram(shader);
+    if (!gl.getProgramParameter(shader, gl.LINK_STATUS)) {
+        throw gl.getProgramInfoLog(shader);
+    }
+    gl.useProgram(shader);
+    
+    //Begin animating
     let template = this;
     let render = function() {
         if (template.isDestroyed) {
@@ -57,7 +81,7 @@ Template.webglView.onRendered(function() {
             sizeDirty = false;
         }
         
-        gl.viewport(0, 0, canvas[0].width, canvas[0].height);
+            gl.viewport(0, 0, canvas[0].width, canvas[0].height);
         gl.clearColor(0.0, 0.192, 0.325, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
         
@@ -68,6 +92,6 @@ Template.webglView.onRendered(function() {
 });
 
 Template.webglView.onDestroyed(function() {
-    $(window).off('resize'); //Destroy resize callbacks.
+    $(window).off('resize'); //Destroy resize callback
     this.isDestroyed = true;
 });
