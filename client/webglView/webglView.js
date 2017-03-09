b1348dcbe14f6e1b08766cc636cd8ed189133133
Sam Johnson <x.smashmaster.x@gmail.com>
2017-03-08 18:29:23 -0700

    Drawing test line

--- client/webglView/webglView.js
+++ client/webglView/webglView.js
@@ -23,15 +23,39 @@ Template.webglView.onCreated(function() {
 });
 
 let VERT_SHADER_SOURCE = "\
-attribute vec2 in_pos;\
-void main(void) {\
-    gl_Position = vec4(in_pos, 0.0, 1.0);\
+attribute vec2 in_pos;\n\
+void main() {\n\
+    gl_PointSize = 16.0;\n\
+    gl_Position = vec4(in_pos, 0.0, 1.0);\n\
 }";
 
-let FRAG_SHADER_SOURCE ="\
-precision mediump float;\
-void main() {\
-    gl_FragColor = vec4(1.0);\
+//Looks complicated and horrible, but this just draws circles.
+let POST_FRAG_SHADER_SOURCE = "\
+#ifdef GL_OES_standard_derivatives\n\
+#extension GL_OES_standard_derivatives : enable\n\
+#endif\n\
+precision mediump float;\n\
+void main() {\n\
+    float alpha = 1.0;\n\
+    vec2 p = gl_PointCoord*2.0 - 1.0;\n\
+    float r = dot(p, p);\n\
+#ifdef GL_OES_standard_derivatives\n\
+    //float delta = fwidth(r);\n\
+    //alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, r);\n\
+    float delta = fwidth(r);\n\
+    alpha = smoothstep(1.0, 1.0 - delta, r);\n\
+#else\n\
+    if (r > 1.0) {\n\
+        discard;\n\
+    }\n\
+#endif\n\
+    gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);\n\
+}";
+
+let LINK_FRAG_SHADER_SOURCE = "\
+precision mediump float;\n\
+void main() {\n\
+    gl_FragColor = vec4(1.0);\n\
 }";
 
 let loadShader = function(gl, source, type) {
@@ -39,7 +63,19 @@ let loadShader = function(gl, source, type) {
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        throw gl.getShaderInfoLog(shader);
+        console.log(gl.getShaderInfoLog(shader));
+    }
+    return shader;
+}
+
+let linkShaderProgram = function(gl, vertShader, fragShader) {
+    let shader = gl.createProgram();
+    gl.attachShader(shader, vertShader);
+    gl.attachShader(shader, fragShader);
+    gl.linkProgram(shader);
+    if (!gl.getProgramParameter(shader, gl.LINK_STATUS)) {
+        console.log(gl.getProgramInfoLog(shader));
+        return null;
     }
     return shader;
 }
@@ -47,6 +83,10 @@ let loadShader = function(gl, source, type) {
 Template.webglView.onRendered(function() {
     let canvas = $(".gl-viewport");
     let gl = canvas[0].getContext('experimental-webgl');
+    gl.clearColor(0.0, 0.192, 0.325, 1.0);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.getExtension('OES_standard_derivatives');
     
     //Set up resize callback
     this.isDestroyed = false;
@@ -58,15 +98,29 @@ Template.webglView.onRendered(function() {
     
     //Set up shader program
     let vertShader = loadShader(gl, VERT_SHADER_SOURCE, gl.VERTEX_SHADER);
-    let fragShader = loadShader(gl, FRAG_SHADER_SOURCE, gl.FRAGMENT_SHADER);
-    let shader = gl.createProgram();
-    gl.attachShader(shader, vertShader);
-    gl.attachShader(shader, fragShader);
-    gl.linkProgram(shader);
-    if (!gl.getProgramParameter(shader, gl.LINK_STATUS)) {
-        throw gl.getProgramInfoLog(shader);
-    }
-    gl.useProgram(shader);
+    let postFragShader = loadShader(gl, POST_FRAG_SHADER_SOURCE, gl.FRAGMENT_SHADER);
+    let linkFragShader = loadShader(gl, LINK_FRAG_SHADER_SOURCE, gl.FRAGMENT_SHADER);
+    let postShader = linkShaderProgram(gl, vertShader, postFragShader);
+    let linkShader = linkShaderProgram(gl, vertShader, linkFragShader);
+    
+    //Set up post vertex buffer
+    let MAX_POSTS = 1000;
+    let MAX_LINKS = 1000;
+    
+    let vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, MAX_POSTS*8, gl.DYNAMIC_DRAW);
+    gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
+    
+    //Set up link index buffer
+    let ebo = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, MAX_LINKS*4, gl.DYNAMIC_DRAW);
+    
+    //Test data
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array([-0.5, -0.5, 0.5, 0.5]));
+    gl.bufferSubData(gl.ELEMENT_ARRAY_BUFFER, 0, new Int16Array([0, 1]));
     
     //Begin animating
     let template = this;
@@ -82,8 +136,11 @@ Template.webglView.onRendered(function() {
             sizeDirty = false;
         }
         
-        gl.clearColor(0.0, 0.192, 0.325, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.useProgram(postShader);
+        gl.drawArrays(gl.POINTS, 0, 2);
+        gl.useProgram(linkShader);
+        gl.drawElements(gl.LINES, 2, gl.UNSIGNED_SHORT, 0);
         
         window.requestAnimationFrame(render);
     }
