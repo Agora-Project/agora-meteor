ff0c6002d6e6e69f65c76c17ae6feb14506c4776
Angle <Ribbonwing@gmail.com>
2017-04-10 21:47:58 -0600

    Fixed edge slant adjustments. Doesn't work well with large graphs, though.

--- server/initial-data.js
+++ server/initial-data.js
@@ -6,12 +6,12 @@
 
 Meteor.startup(function() {
     let DEBUG_RESET = false; //Deletes all posts and add a set of random fake posts.
-    
+
     if (DEBUG_RESET) {
         console.log('Deleting all posts');
         Posts.remove({});
     }
-    
+
     if (!Posts.findOne({$where : '!this.links || this.links.length < 1'})) {
         console.log("Adding root post");
         Posts.insert({
@@ -19,53 +19,53 @@ Meteor.startup(function() {
             links: []
         });
     }
-    
+
     if (DEBUG_RESET) {
         console.log("Adding fake posts");
-        
+
         let posts = [];
-        
+
         let observer = Posts.find({}, {fields: {'_id': 1, 'postedOn': 1}}).observe({
             added: function(post) {
                 posts.push(post);
             }
         });
-        
-        for (let i=0; i<100; i++) {
+
+        for (let i=0; i<10000; i++) {
             //Decrease exponent to more strongly prefer replying to newer posts.
             let random = Math.pow(Math.random(), 0.1);
             let post = posts[Math.floor(random*posts.length)];
-            
+
             let reply = {
                 title: 'Fake Post',
                 links: [{target: post._id}]
             };
-            
+
             if (Math.random() > 0.5) {
                 reply.content = 'Fake content.';
             }
 
             Meteor.call('insertPost', reply);
         }
-        
+
         observer.stop();
     }
-    
+
     //Compute default layout of posts.
     console.log('Laying out posts');
     let posts = {};
     Posts.find({}, {fields: {'_id': 1, 'links': 1}}).forEach(function(post) {
         posts[post._id] = post;
     });
-    
+
     let grapher = new LayeredGrapher(posts);
-    
+
     for (let id in posts) {
         let post = posts[id];
         Posts.update({_id: id},
                     {$set: {defaultPosition: {x:post.x, y:post.y}}});
     }
-    
+
     //Set up moderator account if it does not exist.
     let moderatorEmail = "moderator@example.com";
     if (!Meteor.users.findOne({
