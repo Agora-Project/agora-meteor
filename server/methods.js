212410eabca2876b03b5796660237cdc4258fff9
Sam Johnson <x.smashmaster.x@gmail.com>
2017-04-28 10:48:47 -0600

    Incrementally adding posts to graph
    Need to update client properly

--- server/methods.js
+++ server/methods.js
@@ -21,8 +21,47 @@ Meteor.methods({
             return;
         }
         
+        let target = Posts.findOne({_id: post.target});
+        if (!target) {
+            return;
+        }
+        
+        //Will always insert directly underneath target, shifting existing posts to the right.
+        let y = target.defaultPosition.y - 1;
+        let x = target.defaultPosition.x;
+        post.defaultPosition = {x: x, y: y};
+        
+        //Find the chain of adjacent posts which need to be shifted.
+        let shifting = false;
+        let postsToShift = [];
+        let prevColumn;
+        Posts.find({'defaultPosition.y': y}, {sort: {'defaultPosition.x': 1}}).forEach(function(post) {
+            if (shifting) {
+                if (post.defaultPosition.x > prevColumn + 1) {
+                    shifting = false;
+                }
+            }
+            else if (post.defaultPosition.x === x) {
+                shifting = true;
+            }
+            
+            if (shifting) {
+                postsToShift.push(post);
+            }
+            
+            prevColumn = post.defaultPosition.x;
+        });
+        
+        //Shift found posts one column to the right.
+        for (let post of postsToShift) {
+            let newColumn = post.defaultPosition.x + 1;
+            Posts.update({_id: post._id}, {$set: {'defaultPosition.x': newColumn}});
+        }
+        
+        //Insert new post into position.
         let postId = Posts.insert(post);
         Posts.update({_id: post.target}, {$push: {replies: postId}});
+        
         return postId;
     },
     submitReport: function(report) {
