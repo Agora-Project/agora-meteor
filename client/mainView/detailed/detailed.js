Agora-Project/agora-meteor

commit 874e3a75d13d308f243410719a269e1d40a6d871

Working on post info bar

client/mainView/detailed/detailed.js
@@ -11,17 +11,12 @@ Template.mainDetailedPost.onCreated(function() {
     this.parent = parentView.templateInstance();
     
     //Automatically update data with content, title, user data, etc.
-    this.post = new ReactiveVar();
     let onSubReady = new Notifier();
     this.onRendered = new Notifier();
     
-    this.subscribe('post', this.data._id, {onReady: onSubReady.fulfill});
+    this.subscribe('post', this.data._id, this.data.poster, {onReady: onSubReady.fulfill});
     
     Notifier.all(onSubReady, this.onRendered).onFulfilled(function() {
-        //Populate post data.
-        let post = Posts.findOne({_id: instance.data._id});
-        instance.post.set(post);
-        
         //Fade out spinner and fade in actual post.
         instance.div.children('.main-detailed-post-spinner').fadeOut(100);
         instance.div.children('.main-detailed-post-flex')
@@ -39,10 +34,13 @@ Template.mainDetailedPost.onRendered(function() {
 
 Template.mainDetailedPost.helpers({
     post: function() {
-        return Template.instance().post.get();
+        return Posts.findOne({_id: Template.instance().data._id});
+    },
+    poster: function() {
+        return Meteor.users.findOne({_id: Template.instance().data.poster});
     },
     age: function() {
-        let post = Template.instance().post.get();
+        let post = Posts.findOne({_id: Template.instance().data._id});
         if (post) {
             return new Date(post.postedOn).toDateString();
         }

client/init.js
@@ -11,7 +11,8 @@ Avatar.setOptions({
     customImageProperty: function() {
         var user = this;
         // calculate the image URL here
-        if (user.avatar)
+        if (user.avatar) {
             return user.avatar;
+        }
     }
 });

client/mainView/detailed/detailed.css
@@ -39,6 +39,22 @@
     flex-direction: row;
     padding: 5px 10px 5px 10px;
     border-bottom: 1px solid #ccc;
+    text-align: center;
+}
+
+.main-detailed-post-avatar {
+}
+
+.main-detailed-post-username {
+    display: inline-block;
+    vertical-align: middle;
+    line-height: normal;
+}
+
+.main-detailed-post-age {
+    display: inline-block;
+    vertical-align: middle;
+    line-height: normal;
 }
 
 .main-detailed-post-content {

client/mainView/detailed/detailed.html
@@ -5,10 +5,15 @@
                 <div class="main-detailed-post-title">{{post.title}}</div>
             {{/if}}
             <div class="main-detailed-post-info">
-                {{#if post.user}}
-                    <a href="/forum/user/{{user._id}}">{{post.user.username}}</a>
+                {{#if poster}}
+                    <a class="main-detailed-post-avatar" href="/user/{{poster._id}}">
+                        {{> avatar user=(poster) size="small" shape="rounded"}}
+                    </a>
+                    <a class="main-detailed-post-username" href="/user/{{poster._id}}">
+                        {{poster.username}}
+                    </a>
                 {{/if}}
-                <span>{{age}}</span>
+                <span class="main-detailed-post-age">{{age}}</span>
             </div>
             <div class="main-detailed-post-content">
                 <p>{{post.content}}</p>

server/publish.js
@@ -4,50 +4,51 @@
     License: GPL, Check file LICENSE
 */
 
+//Returns all information about a single post and its poster's basic information.
+Meteor.publish('post', function(postID, posterID) {
+    return [
+        Posts.find({_id: postID}),
+        Meteor.users.find({_id: posterID}, {fields: {username: 1, avatar: 1}})
+    ];
+});
+
+//Returns an abstract shell of all posts, each only containing its id and links.
+Meteor.publish('abstractPosts', function() {
+    return Posts.find({}, {fields: {poster: 1, target: 1, replies: 1, defaultPosition: 1}});
+});
+
+//Universal subscription for roles.
 Meteor.publish(null, function() {
     return Meteor.roles.find({});
 });
 
-Meteor.publish("myself", function() {
+//Returns info about the client user.
+Meteor.publish('myself', function() {
     if (this.userId) {
-        return Meteor.users.find({
-            _id: this.userId
-        }, {
-            fields: {
-                'isBanned': 1,
-                'createdAt': 1
-            }
+        return Meteor.users.find({_id: this.userId}, {
+            fields: {isBanned: 1, createdAt: 1}
         });
     } else {
         return this.ready();
     }
 });
 
-Meteor.publish("users", function() {
+//Users; for the user management page. Should restrict fields even to moderators--shouldn't send user tokens and password hashes over network--ever.
+Meteor.publish('users', function() {
     if (Roles.userIsInRole(this.userId, ['moderator'])) {
         return Meteor.users.find({});
     } else {
         return Meteor.users.find({}, {
-            fields: {_id: 1, username: 1, avatar: 1}
+            fields: {username: 1, avatar: 1}
         });
     }
 });
 
-Meteor.publish("reports", function() {
+//Reports; for the report management page.
+Meteor.publish('reports', function() {
     if (Roles.userIsInRole(this.userId, ['moderator'])) {
         return Reports.find({});
-    } else return this.ready();
+    } else {
+        return this.ready();
+    }
 })
-
-Meteor.publish("post", function(id) {
-    return Posts.find({_id: id});
-});
-
-Meteor.publish("newestPosts", function(beforeDate) {
-    return Posts.find({}, {limit: 1000});
-});
-
-//Returns an abstract shell of posts, each only containing its id and links.
-Meteor.publish("abstractPosts", function() {
-    return Posts.find({}, {fields: {'target': 1, 'replies': 1, 'defaultPosition': 1}});
-});

commit 3b3d5b45caf150e210bec77dad644f3ee11fafb9

Fixed missing reply box
Bug introduced by 874e3a7

client/mainView/detailed/detailed.js
@@ -11,12 +11,18 @@ Template.mainDetailedPost.onCreated(function() {
     this.parent = parentView.templateInstance();
     
     //Automatically update data with content, title, user data, etc.
+    this.post = new ReactiveVar();
+    this.poster = new ReactiveVar();
     let onSubReady = new Notifier();
     this.onRendered = new Notifier();
     
     this.subscribe('post', this.data._id, this.data.poster, {onReady: onSubReady.fulfill});
     
     Notifier.all(onSubReady, this.onRendered).onFulfilled(function() {
+        //Populate post data.
+        instance.post.set(Posts.findOne({_id: instance.data._id}));
+        instance.poster.set(Meteor.users.findOne({_id: instance.data.poster}));
+        
         //Fade out spinner and fade in actual post.
         instance.div.children('.main-detailed-post-spinner').fadeOut(100);
         instance.div.children('.main-detailed-post-flex')
@@ -34,13 +40,13 @@ Template.mainDetailedPost.onRendered(function() {
 
 Template.mainDetailedPost.helpers({
     post: function() {
-        return Posts.findOne({_id: Template.instance().data._id});
+        return Template.instance().post.get();
     },
     poster: function() {
-        return Meteor.users.findOne({_id: Template.instance().data.poster});
+        return Template.instance().poster.get();
     },
     age: function() {
-        let post = Posts.findOne({_id: Template.instance().data._id});
+        let post = Template.instance().post.get();
         if (post) {
             return new Date(post.postedOn).toDateString();
         }
@@ -130,6 +136,6 @@ Template.mainDetailedPostReplyButton.onCreated(function() {
 Template.mainDetailedPostReplyButton.events({
     'click': function(event, instance) {
         //Our parent is a mainDetailedPost, and its parent is the mainView.
-        instance.parent.parent.replyTarget.set(instance.parent.post);
+        instance.parent.parent.replyTarget.set(instance.parent.post.get());
     }
 });

client/mainView/main.js
@@ -178,8 +178,7 @@ Template.mainReply.onCreated(function() {
 
 Template.mainReply.onRendered(function() {
     let instance = this;
-    //No idea why we need curValue here. get() should work on its own but it doesn't.
-    let target = this.parent.replyTarget.get().curValue;
+    let target = this.parent.replyTarget.get();
     
     let div = $('#main-reply');
     div.css('top', -div.outerHeight());