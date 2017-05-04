2f4dae76531da5f226dc9d2acb224218824be482
Sam Johnson <x.smashmaster.x@gmail.com>
2017-05-03 22:41:14 -0600

    Moved reply sources to new files

--- client/mainView/main.js
+++ client/mainView/main.js
@@ -5,7 +5,7 @@
 */
 
 /**
- * The main view consists of five basic modules:
+ * The main view consists of six basic modules:
  *
  *    * Main Module (main.js)
  *          This file; the primary entry-point and handler for the other four modules.
@@ -27,6 +27,9 @@
  *          Handles the creation of template-based HTML posts.
  *          These detailed posts appear when the camera is sufficiently zoomed.
  *          Depends on the camera and partitioner.
+ *
+ *    * Reply (reply/reply.js)
+ *          Handles the reply box and related code.
  *
  */
 
@@ -168,83 +171,4 @@ Template.mainView.onDestroyed(function() {
     this.postObserver.stop();
     this.changeObserver.stop();
     this.isDestroyed = true;
-});
-
-Template.mainReply.onCreated(function() {
-    let parentView = this.view.parentView;
-    while (parentView.templateInstance === undefined) {
-        parentView = parentView.parentView;
-    }
-    this.parent = parentView.templateInstance();
-    
-    this.errorMessage = new ReactiveVar();
-});
-
-Template.mainReply.onRendered(function() {
-    let instance = this;
-    let target = this.parent.replyTarget.get();
-    
-    let div = $('#main-reply');
-    div.css('top', -div.outerHeight());
-    
-    let titleInput = $('#main-reply-title');
-    let contentInput = $('#main-reply-textarea');
-    
-    let hasContent = function() {
-        return titleInput.val().length > 0 || contentInput.val().length > 0;
-    };
-    
-    $('#main-reply-submit-button').click(function(event) {
-        let post = {
-            title: titleInput.val(),
-            content: contentInput.val(),
-            target: target._id
-        };
-
-        Meteor.call("insertPost", post, function(error) {
-            if (error) {
-                //Display error message to user.
-                instance.errorMessage.set(error.reason);
-            }
-            else {
-                //Don't delete user's work unless it posts successfully.
-                instance.parent.replyTarget.set();
-            }
-        });
-    });
-
-    $('#main-reply-cancel-button').click(function(event) {
-        if (!hasContent() || confirm('You have an unfinished post. Are you sure you want to cancel?')) {
-            instance.parent.replyTarget.set();
-        }
-    });
-    
-    $(window).on('beforeunload', function(event) {
-        if (hasContent()) {
-            return 'You have an unfinished post. Are you sure you want to close Agora?';
-        }
-    });
-});
-
-Template.mainReply.helpers({
-    errorMessage: function() {
-        return Template.instance().errorMessage.get();
-    }
-});
-
-Template.mainReply.events({
-    'mousedown, touchstart, mousemove, touchmove, mouseup, touchend, wheel': function(event, instance) {
-        if (instance.parent.camera.isDragging()) {
-            //Prevents interaction while dragging.
-            event.preventDefault();
-        }
-        else {
-            //Prevent events from passing through posts into the WebGL canvas.
-            event.stopImmediatePropagation();
-        }
-    }
-});
-
-Template.mainReply.onDestroyed(function() {
-    $(window).off('beforeunload');
 });
