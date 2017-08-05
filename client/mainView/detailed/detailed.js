5a4acd230e8411773472ea833e880a839eac1e6c
Angle <Ribbonwing@gmail.com>
2017-08-04 23:27:10 -0600

    fixed not showing posts when logged out,.

--- client/mainView/detailed/detailed.js
+++ client/mainView/detailed/detailed.js
@@ -10,7 +10,7 @@ Template.mainDetailedPost.onCreated(function() {
 
     let user = Meteor.users.findOne({_id: Meteor.userId()});
 
-    if (user.seenPosts && user.seenPosts.find(function(postID) {
+    if (!user || user.seenPosts && user.seenPosts.find(function(postID) {
         return postID == instance.data._id;
     }))
         instance.seen = true;
@@ -244,7 +244,7 @@ Template.mainBasicPost.onCreated(function() {
 
     let user = Meteor.users.findOne({_id: Meteor.userId()});
 
-    if (user.seenPosts && user.seenPosts.find(function(postID) {
+    if (!user || user.seenPosts && user.seenPosts.find(function(postID) {
         return postID == instance.data._id;
     }))
         instance.seen = true;
