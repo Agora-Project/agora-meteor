93a2b7db770ef2d204e36ca3b4a1b332d47c049e
Angle <Ribbonwing@gmail.com>
2017-12-18 19:42:16 -0700

    Changed License to AGPL-3.0

--- server/publish.js
+++ server/publish.js
@@ -1,7 +1,7 @@
 /*
     Agora Forum Software
     Copyright (C) 2016 Gregory Sartucci
-    License: GPL, Check file LICENSE
+    License: AGPL-3.0, Check file LICENSE
 */
 
 //Returns all information about a single post and its poster's basic information.
