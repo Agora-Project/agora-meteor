7438029944b51c3a3d44b95e50d2b38505d6fab6
Angle <Ribbonwing@gmail.com>
2018-09-22 16:53:56 -0600

    stop adding "Re: " when replying to your own posts.

--- client/mainView/reply/reply.js
+++ client/mainView/reply/reply.js
@@ -179,7 +179,7 @@ Template.mainReply.onRendered(function() {
         $(window).on('beforeunload', exitReply);
 
         if (target) {
-            if (target.summary && !target.summary.startsWith("Re: "))
+            if (target.attributedTo != Meteor.user().actor && target.summary && !target.summary.startsWith("Re: "))
                 summaryInput.val("Re: " + target.summary);
             else summaryInput.val(target.summary);
 
