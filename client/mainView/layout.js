106d572967d947140a613b13e6a5601bcc50fb80
Angle <Ribbonwing@gmail.com>
2019-02-13 13:54:21 -0700

    refactor boilerplate into function

--- client/mainView/layout.js
+++ client/mainView/layout.js
@@ -5,56 +5,55 @@
 */
 
 MainViewLayout = function() {
-
     localPostPositions = new Mongo.Collection(null);
+
+
+    let finalizeResults = function(results, post) {
+        let changedPosts = new Set();
+
+        for (let id of results.changedPosts.values()) {
+            let updatedPost = localPostPositions.findOne({id: id});
+            if (!updatedPost) console.log("Updated Post not found: ", id);
+            else changedPosts.add(updatedPost);
+        }
+
+        let finalPost = localPostPositions.findOne({id: results.post.id});
+        if (finalPost) results.post = finalPost;
+        else {
+          console.log("No final post: ", post, results.post);
+          console.log("Action type: ", results.type);
+        }
+
+        results.changedPosts = changedPosts;
+
+        return results;
+    }
 
     this.init = function(initPostArray) {
         for (let post of initPostArray) {
             localPostPositions.insert(post);
         }
+
         LayeredGrapher.layoutGraph(localPostPositions);
         return localPostPositions.find({}).fetch();
     };
 
     this.addPost = function(post) {
         let results = LayeredGrapher.insertPost(localPostPositions, post);
-        let changedPosts = new Set();
 
-        for (let id of results.changedPosts.values()) {
-            changedPosts.add(localPostPositions.findOne({_id: id}));
-        }
-
-        results.post = localPostPositions.findOne({id: post.id});
-
-        results.changedPosts = changedPosts;
-
-        return results;
+        return finalizeResults(results, post);
     };
 
     this.repositionPost = function(post) {
       let results = LayeredGrapher.repositionPost(localPostPositions, post);
-      let changedPosts = new Set();
 
-      for (let id of results.changedPosts.values()) {
-          changedPosts.add(localPostPositions.findOne({_id: id}));
-      }
-
-      results.changedPosts = changedPosts;
-
-      return results;
+      return finalizeResults(results, post);
     }
 
     this.removePost = function(post) {
         let results = LayeredGrapher.removePost(localPostPositions, post);
-        let changedPosts = new Set();
 
-        for (let updatedPost_id of results.changedPosts.values()) {
-            changedPosts.add(localPostPositions.findOne({_id: updatedPost_id}));
-        }
-
-        results.changedPosts = changedPosts;
-
-        return results;
+        return finalizeResults(results, post);
     };
 
     this.updatePost = function(_id, fields) {
